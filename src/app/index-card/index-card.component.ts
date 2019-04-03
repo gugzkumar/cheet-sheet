75461c1ab9a09b47a9f847379bd8ab5e6fe601b0
Gagan Tunuguntla <gugzkumar@gmail.com>
2019-04-03 19:48:10 -0400

    add hover to card

--- src/app/index-card/index-card.component.ts
+++ src/app/index-card/index-card.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-index-card',
   templateUrl: './index-card.component.html',
-  styleUrls: ['./index-card.component.sass']
+  styleUrls: ['./index-card.component.scss']
 })
 export class IndexCardComponent implements OnInit {
+  public enableElevation:boolean = false;
 
   constructor() { }
 
