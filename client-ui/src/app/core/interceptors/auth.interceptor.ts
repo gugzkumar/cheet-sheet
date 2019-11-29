db0267f101a350aeb90c80c517b36594449e2b86
Gagan Tunuguntla <gugzkumar@gmail.com>
2019-11-28 22:06:32 -0500

    unused library

--- client-ui/src/app/core/interceptors/auth.interceptor.ts
+++ client-ui/src/app/core/interceptors/auth.interceptor.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { finalize } from "rxjs/operators";
 
 import { AuthService } from '../services/auth.service';
 
