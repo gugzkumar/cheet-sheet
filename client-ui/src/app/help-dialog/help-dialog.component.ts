47d7076314208f5ad1974e2408e62275edfc9b3b
Gagan Tunuguntla <gugzkumar@gmail.com>
2020-01-02 22:55:32 -0500

    hotfix removed duplicate component

--- client-ui/src/app/help-dialog/help-dialog.component.ts
+++ client-ui/src/app/help-dialog/help-dialog.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-
-import {
-    MatDialogRef
-} from '@angular/material';
 
 @Component({
     selector: 'app-help-dialog',
     templateUrl: './help-dialog.component.html',
     styleUrls: ['./help-dialog.component.scss']
 })
-export class HelpDialogComponent implements OnInit {
-
-    constructor(
-        public dialogRef: MatDialogRef<HelpDialogComponent>,
-    ) {
-
-    }
-
-    ngOnInit() {
-    }
-
+export class HelpDialogComponent {
+    constructor() {}
 }
